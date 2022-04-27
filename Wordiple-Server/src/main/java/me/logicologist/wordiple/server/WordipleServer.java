c8eb026e6d4dbad69111725a4b75b0efe380fa5f
Logicologist <thereallogicalyt@gmail.com>
2022-04-27 15:17:22 -0400

    Account creation complete

--- Wordiple-Server/src/main/java/me/logicologist/wordiple/server/WordipleServer.java
+++ Wordiple-Server/src/main/java/me/logicologist/wordiple/server/WordipleServer.java
@@ -5,24 +5,14 @@ import me.logicologist.wordiple.server.managers.DatabaseManager;
 import me.logicologist.wordiple.server.managers.PacketManager;
 import me.logicologist.wordiple.server.managers.SessionManager;
 
+import java.util.Scanner;
+import java.util.UUID;
+
 public class WordipleServer {
 
     public static void main(String[] args) {
         new PacketManager().load();
         new DatabaseManager().setup();
         new SessionManager();
-
-        PacketArguments packetArguments = new PacketArguments()
-                .setArgument("email", String.class)
-                .setArgument("username", String.class)
-                .setArgument("password", String.class)
-                .setArgument("response", String.class);
-
-        packetArguments
-                .setValues("email", "thereallogicalyt@gmail.com")
-                .setValues("username", "Logicologist")
-                .setValues("password", "test");
-
-        SessionManager.getInstance().createSignupSession(packetArguments);
     }
 }
