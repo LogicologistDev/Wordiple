62646bc686c203408f9287e062eaf6ec8bebb326
Logicologist <thereallogicalyt@gmail.com>
2022-04-27 15:17:33 -0400

    Account creation complete

--- Wordiple-Server/src/main/java/me/logicologist/wordiple/server/WordipleServer.java
+++ Wordiple-Server/src/main/java/me/logicologist/wordiple/server/WordipleServer.java
@@ -1,12 +1,8 @@
 package me.logicologist.wordiple.server;
 
-import com.olziedev.olziesocket.framework.PacketArguments;
 import me.logicologist.wordiple.server.managers.DatabaseManager;
 import me.logicologist.wordiple.server.managers.PacketManager;
 import me.logicologist.wordiple.server.managers.SessionManager;
-
-import java.util.Scanner;
-import java.util.UUID;
 
 public class WordipleServer {
 
