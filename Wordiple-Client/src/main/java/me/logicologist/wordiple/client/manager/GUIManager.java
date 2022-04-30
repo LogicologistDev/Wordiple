6f9212d25b539ee55607f845403165a0baa1b43b
LogicologistDev <thereallogicalyt@gmail.com>
2022-04-30 12:43:49 -0400

    guimanager

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/manager/GUIManager.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/manager/GUIManager.java
@@ -1,15 +1,16 @@
-package me.logicologist.wordiple.client.guis;
+package me.logicologist.wordiple.client.manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class MainScreen extends Application {
-
+public class GUIManager extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Wordiple");
+        Font.loadFont(getClass().getResourceAsStream("NexaLight.ttf"), 16);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mainscreen.fxml"));
 
         stage.setScene(new Scene(fxmlLoader.load()));
