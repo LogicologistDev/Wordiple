5e5a13376608d6a2b50c5b46d48284b326d4fe39
LogicologistDev <thereallogicalyt@gmail.com>
2022-05-02 22:44:15 -0400

    ButtonSkin finished

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/gui/buttonskins/NormalSkin.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/gui/buttonskins/NormalSkin.java
@@ -1,6 +1,9 @@
 package me.logicologist.wordiple.client.gui.buttonskins;
 
-import javafx.animation.*;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.scene.control.Button;
 import javafx.scene.control.skin.ButtonSkin;
 import javafx.util.Duration;
@@ -11,21 +14,26 @@ public class NormalSkin extends ButtonSkin {
     private boolean pressed;
 
     private boolean exited;
+
     public NormalSkin(Button button) {
         super(button);
 
         FadeTransition fadeIn = new FadeTransition(Duration.millis(25));
         fadeIn.setNode(button);
-        fadeIn.setToValue(1);
+        fadeIn.setToValue(0.8);
 
-        FadeTransition fadeOut = new FadeTransition(Duration.millis(100));
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(50));
         fadeOut.setNode(button);
-        fadeOut.setToValue(0.9);
+        fadeOut.setToValue(0.6);
+
+        FadeTransition fadePress = new FadeTransition(Duration.millis(10));
+        fadePress.setNode(button);
+        fadePress.setToValue(1);
 
         final Timeline timelineOut = new Timeline();
         timelineOut.setCycleCount(1);
         final KeyValue kvOut = new KeyValue(button.layoutYProperty(), button.getLayoutY());
-        final KeyFrame kfOut = new KeyFrame(Duration.millis(25), kvOut);
+        final KeyFrame kfOut = new KeyFrame(Duration.millis(50), kvOut);
         timelineOut.getKeyFrames().add(kfOut);
         timelineOut.setAutoReverse(false);
 
@@ -38,13 +46,12 @@ public class NormalSkin extends ButtonSkin {
 
         final Timeline timelinePress = new Timeline();
         timelinePress.setCycleCount(1);
-        final KeyValue kvPress = new KeyValue(button.layoutYProperty(), button.getLayoutY() + 20);
+        final KeyValue kvPress = new KeyValue(button.layoutYProperty(), button.getLayoutY() + 15);
         final KeyFrame kfPress = new KeyFrame(Duration.millis(25), kvPress);
         timelinePress.getKeyFrames().add(kfPress);
         timelinePress.setAutoReverse(false);
 
         button.setOnMouseEntered(e -> {
-            System.out.println(button.getLayoutY());
             fadeIn.playFromStart();
             timelineIn.play();
             timelineOut.stop();
@@ -61,17 +68,23 @@ public class NormalSkin extends ButtonSkin {
 
         button.setOnMousePressed(e -> {
             timelinePress.play();
+            fadePress.playFromStart();
             pressed = true;
         });
 
         button.setOnMouseReleased(e -> {
             timelinePress.stop();
             pressed = false;
-            if (!exited) timelineIn.play();
-            if (exited) timelineOut.play();
+            if (!exited) {
+                timelineIn.play();
+                fadeIn.play();
+                return;
+            }
+            timelineOut.play();
+            fadeOut.play();
         });
 
-        button.setOpacity(0.8);
+        button.setOpacity(0.6);
     }
 
 
