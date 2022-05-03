5fef5d3c5f707c717195b22681c235e057c94e84
LogicologistDev <thereallogicalyt@gmail.com>
2022-05-02 23:13:53 -0400

    max window size

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/manager/GUIManager.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/manager/GUIManager.java
@@ -9,6 +9,10 @@ public class GUIManager extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Wordiple");
+        stage.setMaxHeight(1080);
+        stage.setMaxWidth(1920);
+        stage.setHeight(1080);
+        stage.setWidth(1920);
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/mainscreen.fxml"));
         stage.setScene(new Scene(fxmlLoader.load()));
         stage.show();
