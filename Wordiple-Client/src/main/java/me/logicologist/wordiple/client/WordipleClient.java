22d07feea878f8bc19fb2b44caaefe991cfb1ef3
LogicologistDev <thereallogicalyt@gmail.com>
2022-05-04 01:44:00 -0400

    buttons

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/WordipleClient.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/WordipleClient.java
@@ -3,9 +3,6 @@ package me.logicologist.wordiple.client;
 import me.logicologist.wordiple.client.manager.GUIManager;
 import me.logicologist.wordiple.client.manager.PacketManager;
 import me.logicologist.wordiple.client.manager.SessionManager;
-import me.logicologist.wordiple.client.packets.UserInfoPacket;
-
-import java.util.concurrent.TimeUnit;
 
 public class WordipleClient {
 
