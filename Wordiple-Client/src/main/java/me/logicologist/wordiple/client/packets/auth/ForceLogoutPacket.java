6a02bcc64f0640329a6167c3008f73d170a9b35b
LogicologistDev <thereallogicalyt@gmail.com>
2022-05-07 02:41:07 -0400

    thread

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/packets/auth/ForceLogoutPacket.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/packets/auth/ForceLogoutPacket.java
@@ -3,6 +3,7 @@ package me.logicologist.wordiple.client.packets.auth;
 import com.olziedev.olziesocket.framework.PacketArguments;
 import com.olziedev.olziesocket.framework.api.packet.PacketAdapter;
 import com.olziedev.olziesocket.framework.api.packet.PacketType;
+import javafx.application.Platform;
 import me.logicologist.wordiple.client.WordipleClient;
 import me.logicologist.wordiple.client.gui.controllers.LoadScreenController;
 import me.logicologist.wordiple.client.manager.GUIManager;
@@ -20,11 +21,13 @@ public class ForceLogoutPacket extends PacketAdapter implements PacketType {
     @Override
     public void onReceive(PacketArguments packetArguments) {
         GUIManager guiManager = GUIManager.getInstance();
-        GUIManager.getInstance().startSwipeTransition(null, () -> {
-            guiManager.showMainScreen(false);
-            LoadScreenController lsc = guiManager.showLoadScreen("Connection has been lost!");
-            SessionManager.getInstance().setLocalSessionID(null);
-            WordipleClient.getExecutor().schedule(() -> lsc.remove(null), 2, TimeUnit.SECONDS);
+        Platform.runLater(() -> {
+            GUIManager.getInstance().startSwipeTransition(null, () -> {
+                guiManager.showMainScreen(false);
+                LoadScreenController lsc = guiManager.showLoadScreen("Connection has been lost!");
+                SessionManager.getInstance().setLocalSessionID(null);
+                WordipleClient.getExecutor().schedule(() -> lsc.remove(null), 2, TimeUnit.SECONDS);
+            });
         });
     }
 
