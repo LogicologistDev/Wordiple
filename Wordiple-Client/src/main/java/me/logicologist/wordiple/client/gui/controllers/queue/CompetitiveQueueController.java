567b829b008f5644815d05a3dc20a36a96d0c006
Olzie-12 <oliver.bolter@virginmedia.com>
2022-05-08 18:35:01 +0100

    session thing

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/gui/controllers/queue/CompetitiveQueueController.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/gui/controllers/queue/CompetitiveQueueController.java
@@ -1,15 +1,21 @@
 package me.logicologist.wordiple.client.gui.controllers.queue;
 
 import com.olziedev.olziesocket.framework.PacketArguments;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import me.logicologist.wordiple.client.WordipleClient;
 import me.logicologist.wordiple.client.gui.controllers.transitions.FadeHorizontalTransitionAdapter;
 import me.logicologist.wordiple.client.manager.GUIManager;
+import me.logicologist.wordiple.common.utils.Utils;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CompetitiveQueueController extends FadeHorizontalTransitionAdapter {
 
@@ -35,6 +41,7 @@ public class CompetitiveQueueController extends FadeHorizontalTransitionAdapter 
     private Label ratingLabel;
 
     private boolean midAction = false;
+    private ScheduledFuture<?> future = null;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -47,8 +54,15 @@ public class CompetitiveQueueController extends FadeHorizontalTransitionAdapter 
             super.transitionOut(() -> {
                 GUIManager.getInstance().showGameSelectScreen(true);
             });
+            future.cancel(true);
         });
+        AtomicLong openedTime = new AtomicLong();
+        future = WordipleClient.getExecutor().scheduleAtFixedRate(() -> {
+            Platform.runLater(() -> timeLabel.setText("Queue Elapsed: " + Utils.formatShortTime(openedTime.getAndIncrement())));
+        }, 0, 1, TimeUnit.SECONDS);
     }
+
+
 
     public void setInfo(PacketArguments playerInfo, PacketArguments queueInfo) {
         activeLabel.setText(queueInfo.get("active", Integer.class) + " Active");
