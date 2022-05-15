4436067adb1704e71755aed3dfec9e5cb112c71b
LogicologistDev <thereallogicalyt@gmail.com>
2022-05-15 03:17:05 -0400

    update fix

--- Wordiple-Client/src/main/java/me/logicologist/wordiple/client/packets/game/GameOverlayPacket.java
+++ Wordiple-Client/src/main/java/me/logicologist/wordiple/client/packets/game/GameOverlayPacket.java
@@ -3,6 +3,7 @@ package me.logicologist.wordiple.client.packets.game;
 import com.olziedev.olziesocket.framework.PacketArguments;
 import com.olziedev.olziesocket.framework.api.packet.PacketAdapter;
 import com.olziedev.olziesocket.framework.api.packet.PacketType;
+import javafx.application.Platform;
 import me.logicologist.wordiple.client.manager.GUIManager;
 
 public class GameOverlayPacket extends PacketAdapter implements PacketType {
@@ -20,7 +21,9 @@ public class GameOverlayPacket extends PacketAdapter implements PacketType {
 
     @Override
     public void onReceive(PacketArguments packetArguments) {
-        GUIManager.getInstance().showGameTextOverlay(packetArguments.get("display", String.class));
+        Platform.runLater(() -> {
+            GUIManager.getInstance().showGameTextOverlay(packetArguments.get("display", String.class));
+        });
     }
 
     @Override
